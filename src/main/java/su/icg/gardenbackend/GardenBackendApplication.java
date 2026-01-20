45ca541805235552bec874f4bea37c6f9f879afc
pilliaredrw <pilliaredrw@gmail.com>
2026-01-20 10:13:13 +0800

    init

---
+++ src/main/java/su/icg/gardenbackend/GardenBackendApplication.java
@@ -0,0 +1,13 @@
+package su.icg.gardenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenBackendApplication.class, args);
+    }
+
+}
